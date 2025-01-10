d3479356333cafb8eb2a9a8d99d5c31a7a6b4a14
bobbygrdn <bobbygrdn777@gmail.com>
2025-01-09 22:01:31 -0800

    feat: updates the code to display our temporary countdown timer

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { ComingSoon } from './components/comingSoon/ComingSoon';
+
+function App() {
+
+  return (
+    <>
+      <ComingSoon />
+    </>
+  )
+}
+
+export default App
