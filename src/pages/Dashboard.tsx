836a7b3390818c36b1feef691c45c66d3803f288
bobbygrdn <bobbygrdn777@gmail.com>
2025-02-01 20:17:54 -0800

    feat: enhance Dashboard component with routing for nested pages

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,28 @@
-export const Dashboard = () => {
-  return <div>Dashboard</div>;
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import { WeeklyMenus } from "./WeeklyMenus";
+import { BrowseDishes } from "./BrowseDishes";
+import { Allergies } from "./Allergies";
+import { Settings } from "./Settings";
+import { DailyMenu } from "./DailyMenu";
+
+export const Dashboard: React.FC = () => {
+  return (
+    <div id="dashboard">
+      {/* <Header /> */}
+      {/* <Sidebar /> */}
+      <div id="content">
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="manage-menus" element={<WeeklyMenus />} />
+          <Route path="manage-menus/daily/:id" element={<DailyMenu />} />
+          <Route path="dishes" element={<BrowseDishes />} />
+          <Route path="allergies" element={<Allergies />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="*" element={<Navigate to="/dashboard/home" />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
